fcb0aa4dad0e26340174c8dde54b55a84587e33e
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-02-28 04:06:10 -0800

    Side bar styles added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const buttonMenu = document.querySelector(".burger-menu");
+const navItem = document.querySelector(".nav-items-container");
 
+console.log(navItem);
+
+buttonMenu.addEventListener("click", () => {
+  navItem.classList.toggle("nav-item-on");
+  navItem.classList.toggle("nav-items-container");
+});
